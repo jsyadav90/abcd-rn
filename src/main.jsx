cb581e6c4c773c4b19edfd1540fa07303db86a00
Jitender <jitenderyadav330@gmail.com>
2026-02-03 09:29:45 +0530

    refactor: Comment out axios import and configuration for cookie handling

--- src/main.jsx
+++ src/main.jsx
@@ -16,13 +16,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios"
 
 import "./index.css";
 import App from "./App.jsx";
 
 // ✅ IMPORTANT: allow cookies in requests
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 // (optional but recommended)
 // axios.defaults.baseURL = "http://localhost:5000";
